3d02c0ab7d8a63c5d28396544455cbae50b4ec28
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2023-09-09 13:56:22 +0700

    edit: footer

--- resources/views/user/help/index.blade.php
+++ resources/views/user/help/index.blade.php
@@ -2,7 +2,7 @@
 
 
     {{-- Question --}}
-    <section>
+    <section class="pt-12 pb-24">
         <div class="flex flex-col justify-center flex-1 px-4 mx-auto mt-28 lg:flex-none max-w-7x">
             <div class="text-center">
 
@@ -19,10 +19,10 @@
             <div class="">
                 <div id="accordion-arrow-icon" data-accordion="open" class="mx-4 md:mx-0">
 
-                    <h2 id="accordion-arrow-icon-heading" class="rounded-lg mb-3 bg-white" data-aos="fade-up"
+                    <h2 id="accordion-arrow-icon-heading" class="rounded-full mb-3 bg-white" data-aos="fade-up"
                         data-aos-duration="1000">
                         <button type="button"
-                            class="flex items-center text-sm font-medium text-xs 2xl:text-sm justify-between w-full p-5 text-left rounded-lg text-black focus:ring-4 focus:ring-gray-200"
+                            class="flex items-center text-sm font-medium text-xs 2xl:text-sm justify-between w-full p-5 text-left rounded-full text-black focus:ring-4 focus:ring-gray-200"
                             data-accordion-target="#accordion-arrow-icon-body-1" aria-expanded="false"
                             aria-controls="accordion-arrow-icon-body">
                             <span>Apa Itu NaZMalogy?</span>
@@ -32,8 +32,8 @@
                     </h2>
                     <div id="accordion-arrow-icon-body-1" class="hidden mb-4"
                         aria-labelledby="accordion-arrow-icon-heading">
-                        <div class="p-5 bg-white rounded-lg">
-                            <p class="text-[#464646] text-xs 2xl:text-md line">
+                        <div class="p-5 bg-white rounded-full">
+                            <p class="text-[#464646] text-xs 2xl:text-sm leading-6">
                                 NaZMaLogy adalah Learning Management System (LMS) yang dibangun oleh NaZMa Office yang
                                 memudahkan pengguna dalam pembelajaran jarak jauh. Dengan platform media pembelajaran
                                 berbasis website yang menyediakan video yang inovatif dan interaktif diharapkan
@@ -42,10 +42,10 @@
                         </div>
                     </div>
 
-                    <h2 id="accordion-arrow-icon-heading" class="rounded-lg mb-3 bg-white" data-aos="fade-up"
+                    <h2 id="accordion-arrow-icon-heading" class="rounded-full mb-3 bg-white" data-aos="fade-up"
                         data-aos-duration="1000">
                         <button type="button"
-                            class="flex items-center text-sm font-medium text-xs 2xl:text-sm justify-between w-full p-5 text-left rounded-lg text-black focus:ring-4 focus:ring-gray-200"
+                            class="flex items-center text-sm font-medium text-xs 2xl:text-sm justify-between w-full p-5 text-left rounded-full text-black focus:ring-4 focus:ring-gray-200"
                             data-accordion-target="#accordion-arrow-icon-body-2" aria-expanded="false"
                             aria-controls="accordion-arrow-icon-body-2">
                             <span>Apa yang saya dapatkan dari NaZMaLogy?</span>
@@ -55,8 +55,8 @@
                     </h2>
                     <div id="accordion-arrow-icon-body-2" class="hidden mb-4"
                         aria-labelledby="accordion-arrow-icon-heading">
-                        <div class="p-5 bg-white rounded-lg">
-                            <p class="text-[#464646] text-xs 2xl:text-md line">
+                        <div class="p-5 bg-white rounded-full">
+                            <p class="text-[#464646] text-xs 2xl:text-sm leading-6">
                                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. A molestias quasi iusto
                                 voluptas, repellat voluptates at quaerat ea error doloribus libero hic earum eum ullam
                                 harum unde architecto accusantium explicabo!
@@ -64,10 +64,10 @@
                         </div>
                     </div>
 
-                    <h2 id="accordion-arrow-icon-heading" class="rounded-lg mb-3 bg-white" data-aos="fade-up"
+                    <h2 id="accordion-arrow-icon-heading" class="rounded-full mb-3 bg-white" data-aos="fade-up"
                         data-aos-duration="1000">
                         <button type="button"
-                            class="flex items-center text-sm font-medium text-xs 2xl:text-sm justify-between w-full p-5 text-left rounded-lg text-black focus:ring-4 focus:ring-gray-200"
+                            class="flex items-center text-sm font-medium text-xs 2xl:text-sm justify-between w-full p-5 text-left rounded-full text-black focus:ring-4 focus:ring-gray-200"
                             data-accordion-target="#accordion-arrow-icon-body-3" aria-expanded="false"
                             aria-controls="accordion-arrow-icon-body">
                             <span>Bagaimana cara mengakses kursus berbayar?</span>
@@ -77,8 +77,8 @@
                     </h2>
                     <div id="accordion-arrow-icon-body-3" class="hidden mb-4"
                         aria-labelledby="accordion-arrow-icon-heading">
-                        <div class="p-5 bg-white rounded-lg">
-                            <p class="text-[#464646] text-xs 2xl:text-md line">
+                        <div class="p-5 bg-white rounded-full">
+                            <p class="text-[#464646] text-xs 2xl:text-sm leading-6">
                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam quaerat commodi
                                 consequatur nesciunt ut vel officia quidem laboriosam eius odit, earum voluptatibus quo
                                 tenetur dicta, architecto aut, ab voluptas a.
@@ -89,7 +89,7 @@
                 </div>
             </div>
         </div>
-        <div class="text-center my-24">
+        {{-- <div class="text-center my-24">
             <h1
                 class="font-bold text-2xl mt-20 md:text-3xl xl:text-4xl tracking-tighter leading-snug max-w-md mx-auto ">
                 Masih Ada Pertanyaan?
@@ -98,7 +98,7 @@
                 class="text-white bg-[#ED7F22] hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium w-48 rounded-full text-sm 2xl:text-sm px-4 py-3 text-center mt-4 mr-3 md:mr-0">Kontak
                 Kami
             </button>
-        </div>
+        </div> --}}
 
 
 
--- resources/views/user/layouts/footer.blade.php
+++ resources/views/user/layouts/footer.blade.php
@@ -1,159 +1,53 @@
-<footer class="bg-white" aria-labelledby="footer-heading">
-    <h2 id="footer-heading" class="sr-only">Footer</h2>
-    <div class="px-4 py-12 mx-auto max-w-7xl sm:px-4">
-        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-            <div class="text-white xl:col-span-1">
-                <a href="/"
-                    class="text-lg font-bold tracking-tighter text-black transition duration-500 ease-in-out transform tracking-relaxed lg:pr-8">
-                    <img src="{{ asset('assets/logo.svg') }}" alt="">
-                </a>
-            </div>
-            <div class="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
-                <div class="md:grid md:grid-cols-2 md:gap-8">
+<footer class="bg-white">
+    <div class="px-4 py-12 mx-auto max-w-7xl">
+        <div class="flex flex-col items-start justify-between pt-16 pb-6 gap-y-12 lg:flex-row lg:items-center lg:py-16">
+            <div>
+                <div class="flex items-center text-black">
                     <div>
-                        <h3 class="font-semibold leading-6 text-black uppercase">
-                            Navigation
-                        </h3>
-                        <ul role="list" class="mt-4 space-y-2">
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Pricing
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    All UI Kits
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Custom pages
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Next.js
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Gatsby
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Remix
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Alpine.js
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Svelte
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    About
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="mt-12 md:mt-0">
-                        <h3 class="font-semibold leading-6 text-black uppercase">
-                            UI/UX &amp; Dev
-                        </h3>
-                        <ul role="list" class="mt-4 space-y-2">
-                            <li>
-                                <a href="https://www.wickedbackgrounds.com/"
-                                    class="text-sm text-gray-500 hover:text-blue-600">
-                                    Wicked Backgrounds
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="https://www.colorsandfonts.com/"
-                                    class="text-sm text-gray-500 hover:text-blue-600">
-                                    Colors &amp; Fonts
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.serendipitytheme.com/"
-                                    class="text-sm text-gray-500 hover:text-blue-600">
-                                    Serendipity</a>
-                            </li>
-                            <li>
-                                <a href="https://www.brutalist.one/" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Brutalist One</a>
-                            </li>
-                        </ul>
+                        <p class="font-semibold leading-6 text-black uppercase">
+                            NaZMalogy
+                        </p>
+                        <p class="mt-1 text-sm">
+                            We'll finish it with exellence
+                        </p>
                     </div>
                 </div>
-                <div class="md:grid md:grid-cols-2 md:gap-8">
-                    <div>
-                        <h3 class="font-semibold leading-6 text-black uppercase">
-                            Legal
-                        </h3>
-                        <ul role="list" class="mt-4 space-y-2">
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Changelog
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    FAQ
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Refund
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    License
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Privacy Policy
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Terms
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="mt-12 md:mt-0">
-                        <h3 class="font-semibold leading-6 text-black uppercase">
-                            Socials
-                        </h3>
-                        <ul role="list" class="mt-4 space-y-2">
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Twitter
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Dribbble
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm text-gray-500 hover:text-blue-600">
-                                    Indie Hackers
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                <nav class="flex gap-8 mt-11">
+                    <a class="relative -my-2 -mx-3 rounded-lg px-3 py-2 text-sm text-gray-500 hover:text-blue-600 transition-colors delay-150 hover:delay-[0ms]"
+                        href="/"><span class="relative z-10">Beranda</span></a><a
+                        class="relative -my-2 -mx-3 rounded-lg px-3 py-2 text-sm text-gray-500 hover:text-blue-600 transition-colors delay-150 hover:delay-[0ms]"
+                        href="{{ route('course.index') }}"><span class="relative z-10">Kursus</span></a><a
+                        class="relative -my-2 -mx-3 rounded-lg px-3 py-2 text-sm text-gray-500 hover:text-blue-600 transition-colors delay-150 hover:delay-[0ms]"
+                        href="{{ route('user.help.index') }}"><span class="relative z-10">Bantuan</span></a>
+                </nav>
+            </div>
+            <div
+                class="relative flex items-center self-stretch p-4 -mx-4 transition-colors group hover:bg-gray-100 sm:self-auto sm:rounded-2xl lg:mx-0 lg:self-auto lg:p-6">
+                {{-- <div class="relative flex items-center justify-center flex-none w-24 h-24 bg-black rounded-xl">
+                    <svg class="w-5 h-5" viewBox="0 0 232 232" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M166.524 51.4683L116.367 101.625L65.5235 51.4683L116.367 0.62434L166.524 51.4683ZM231.11 116.054L180.953 166.898L130.796 116.054L180.953 65.8969L231.11 116.054ZM101.939 116.054L51.0948 166.898L0.250934 116.054L51.0948 65.8969L101.939 116.054ZM166.524 181.326L116.367 231.483L65.5235 181.326L116.367 130.482L166.524 181.326Z"
+                            fill="#0c0c0c"></path>
+                    </svg>
+                </div> --}}
+                <div class="ml-8 lg:w-64">
+                    <p class="text-base font-semibold text-black">
+                        <a href="#"><span class="absolute inset-0 sm:rounded-2xl"></span>
+                            Tetap terhubung dengan kami
+                        </a>
+                    </p>
+                    <p class="mt-1 text-sm text-gray-500 hover:text-blue-600">
+                        Ikuti kami di media sosial
+                    </p>
                 </div>
             </div>
+        </div>
+        <div class="text-center pt-8 pb-12 md:flex-row-reverse md:justify-between md:pt-6">
+            <p class="mt-6 text-sm text-gray-500 md:mt-0">
+                © Copyright
+                <!-- -->{{ date('Y') }}
+                <!-- -->. All rights reserved.
+            </p>
         </div>
     </div>
 </footer>
