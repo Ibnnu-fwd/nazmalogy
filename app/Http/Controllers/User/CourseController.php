9c86ab40b51236f6b78c5ab591f79035b8195203
dyooo123 <aldyoputra21@gmail.com>
2023-09-20 20:17:17 +0700

    edit : course page backend integrate

--- app/Http/Controllers/User/CourseController.php
+++ app/Http/Controllers/User/CourseController.php
@@ -4,23 +4,29 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\CourseInterface;
+use App\Interfaces\CourseCategoryInterface;
 use App\Interfaces\TransactionInterface;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
     private $course;
+    private $courseCategory;
     private $transaction;
 
-    public function __construct(CourseInterface $course, TransactionInterface $transaction)
+    public function __construct(CourseInterface $course, TransactionInterface $transaction, CourseCategoryInterface $courseCategory)
     {
         $this->course      = $course;
+        $this->courseCategory = $courseCategory;
         $this->transaction = $transaction;
     }
 
     public function index()
     {
-        return view('user.course.index');
+        return view('user.course.index',[
+            'courseCategories'    => $this->courseCategory->getAll(),
+            'courses'             => $this->course->getAll(),
+        ]);
     }
 
     public function show($id)
--- resources/views/user/course/index.blade.php
+++ resources/views/user/course/index.blade.php
@@ -17,7 +17,7 @@
         <div class="mx-auto px-4 mb-12 max-w-7xl grid grid-cols-1 gap-y-6 lg:gap-6 lg:grid-cols-4">
             <div class="bg-white rounded-xl p-4 h-fit">
                 <div class="mb-6">
-                    <input type="text" id="default-input"
+                    <input type="text" id="search"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-xs 2xl:text-sm rounded-xl focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5"
                         placeholder="Cari">
                 </div>
@@ -100,52 +100,33 @@
             </div>
             <div class="col-span-3">
                 <div class="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-3">
-
-                    @for ($i = 1; $i <= 8; $i++)
-                        <div class="max-w-sm bg-white rounded-xl border border-gray-100">
-                            <a>
-                                <img class="rounded-t-xl h-40 object-center object-cover w-full"
-                                    src="{{ asset('assets/noimage.svg') }}" alt="" />
-                            </a>
-                            <div class="p-5">
-                                <div class="flex items-center justify-between mb-3">
-                                    <div class="flex items-center gap-x-2">
-                                        <div class="w-2.5 h-2.5 rounded-full bg-primary"></div>
-                                        <span class="text-xs 2xl:text-sm font-medium">
-                                            E-Bussiness
-                                        </span>
-                                    </div>
-                                    <div class="flex items-center">
-                                        <ion-icon name="star"
-                                            class="text-[#F3AB1D] rounded mr-1 w-[12] h-[12]"></ion-icon>
-                                        <span class="text-xs 2xl:text-sm font-medium">
-                                            (5.0)
-                                        </span>
-                                    </div>
-                                </div>
-                                <a href="#">
-                                    <h5
-                                        class="mb-2 text-xs 2xl:text-md font-bold tracking-tight text-gray-900 dark:text-white line-clamp-2 hover:line-clamp-none">
-                                        Mengenal Project Network Planning
-                                    </h5>
-                                </a>
-                                <div class="flex items-end justify-between mt-4">
-                                    <div>
-                                        <p class="text-xs 2xl:text-sm line-through text-danger">Rp. 500.000</p>
-                                        <p class="text-xs 2xl:text-md font-semibold">Rp. 75.000</p>
-                                    </div>
-                                    <p class="text-gray-400 text-xs 2xl:text-sm flex items-center">
-                                        <ion-icon name="person-circle-outline"
-                                            class="text-gray-400 mr-1 w-4 h-4"></ion-icon>
-                                        <span>125</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    @endfor
-
+                   
+                    
                 </div>
             </div>
         </div>
     </section>
+
+    @push('js-internal')
+        <script>
+            const search = document.getElementById('search');
+            const course = document.querySelectorAll('.max-w-sm');
+
+            search.addEventListener('keyup', function(e) {
+                const query = e.target.value.toLowerCase();
+
+                course.forEach(course => {
+                    const name = course.querySelector('h5').innerText.toLowerCase();
+                    const category = course.querySelector('.text-xs').innerText.toLowerCase();
+
+                    if (name.includes(query) || category.includes(query)) {
+                        course.style.display = 'block';
+                    } else {
+                        course.style.display = 'none';
+                    }
+                })
+            })
+
+        </script>
+    @endpush
 </x-guest-layout>
