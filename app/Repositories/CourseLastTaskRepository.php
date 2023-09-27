442f59d26859138e1fbe69993c2a21910dbf9f40
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2023-09-27 15:16:55 +0700

    sdflkj

--- app/Repositories/CourseLastTaskRepository.php
+++ app/Repositories/CourseLastTaskRepository.php
@@ -58,13 +58,13 @@ class CourseLastTaskRepository implements CourseLastTaskInterface
     public function attempt($id, $data)
     {
         // Find the submission record
-        $submission = $this->submission->with('courseLastTask')->where('course_last_task_id', $id)->first();
+        $submission = $this->submission->with('course_last_task')->where('course_last_task_id', $id)->first();
 
         // Get the point type for submissions
         $pointType = PointType::where('name', 'submission')->first();
 
         // Define the description for points
-        $description = 'Submission ' . $submission->courseLastTask->name;
+        $description = 'Submission ' . $submission->course_last_task->name;
 
         // Check if a point record exists, and create one if not
         if (!$submission) {
